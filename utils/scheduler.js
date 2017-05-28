e9e128098b85e1a739e0949c4f1a5166d9910d4b
Medinilla <cmedinilla@hpe.com>
2017-05-28 14:58:40 -0500

    Fix manco tronques

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -2,9 +2,9 @@ const
   cron = require('cron').CronJob,
   Job = require('../routes/jobCollection');
 
-exports.newJob((id, currPrice, query) => {
-  new CronJob(
+exports.newJob=(id, currPrice, query) => {
+  new cron(
     '* * */12 * * *', 
     Job.Checar_Precio(id, currPrice, query), 
     null, true, 'America/Los_Angeles');
-});
+};
